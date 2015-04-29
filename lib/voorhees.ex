c45b708b9aebf48eb6fe16e5325a304c2f8aa109
Dan McClain <git@danmcclain.net>
2015-04-29 11:45:41 -0400

    Handles nested key matching

--- lib/voorhees.ex
+++ lib/voorhees.ex
@@ -1,7 +1,10 @@
 defmodule Voorhees do
+  require IEx
   def matches_schema?(content, keys) do
-    content_keys = Poison.decode!(content)
+    content_map =  Poison.decode!(content)
+    content_keys = content_map
     |> Map.keys
+    |> _extract_subkeys(content_map)
     |> _normalize_keys
 
     keys = _normalize_keys(keys)
@@ -12,4 +15,18 @@ defmodule Voorhees do
   defp _normalize_keys([]), do: []
   defp _normalize_keys([key|rest]) when is_atom(key), do: [key|_normalize_keys(rest)]
   defp _normalize_keys([key|rest]) when is_binary(key), do: [String.to_atom(key)|_normalize_keys(rest)]
+  defp _normalize_keys([{key, subkeys}|rest]) when is_atom(key) and is_list(subkeys) do
+    [{key, _normalize_keys(subkeys)}|_normalize_keys(rest)]
+  end
+  defp _normalize_keys([{key, subkeys}|rest]) when is_binary(key) and is_list(subkeys) do
+    [{String.to_atom(key), _normalize_keys(subkeys)}|_normalize_keys(rest)]
+  end
+
+  defp _extract_subkeys([], _map), do: []
+  defp _extract_subkeys([key|rest], map) do
+    case map[key] do
+      (value) when is_map(value) -> [{key, _extract_subkeys(Map.keys(value), value)}|_extract_subkeys(rest, map)]
+      (_) -> [key|_extract_subkeys(rest, map)]
+    end
+  end
 end
--- test/matches_schema_test.exs
+++ test/matches_schema_test.exs
@@ -3,14 +3,34 @@ defmodule Voorhees.Test.MatchesSchema do
   import Voorhees
 
   test "desired keys can be either strings or atoms" do
-    assert matches_schema?("{ \"a\": 1, \"b\": 2 }", [:a, "b"])
+    content = Poison.encode! %{ a: 1, b: 2 }
+    assert matches_schema?(content, [:a, "b"])
   end
 
   test "returns false when a key is missing from the passed in value" do
-    assert !matches_schema?("{ \"a\": 1 }", [:a, :b])
+    content = Poison.encode! %{ a: 1 }
+    assert !matches_schema?(content, [:a, :b])
   end
 
   test "returns false when an extra key is present in the the passed in value" do
-    assert !matches_schema?("{ \"a\": 1, \"b\": 2, \"c\": 3 }", [:a, :b])
+    content = Poison.encode! %{ a: 1, b: 2, c: 3 }
+    assert !matches_schema?(content, [:a, :b])
+  end
+
+  test "checks nested keys" do
+    content = Poison.encode! %{ a: 1, b: 2, c: %{ d: 4 } }
+    assert matches_schema?(content, [:a, :b, c: [:d]])
+
+    content = Poison.encode! %{ a: 1, b: 2, c: %{ d: 4, e: %{ f: 5 } } }
+    assert matches_schema?(content, [:a, :b, c: [:d, e: [:f]]])
+
+    content = Poison.encode! %{ a: 1, b: 2, c: %{ d: 4, e: 6 } }
+    assert !matches_schema?(content, [:a, :b, c: [:d, e: [:f]]])
+
+    content = Poison.encode! %{ a: 1, b: 2, c: %{ d: 4, e: 6, f: 7 } }
+    assert !matches_schema?(content, [:a, :b, c: [:d, e: [:f]]])
+
+    content = Poison.encode! %{ a: 1, b: 2, c: %{ d: 4, e: %{ f: 7, g: 8 } } }
+    assert !matches_schema?(content, [:a, :b, c: [:d, e: [:f]]])
   end
 end
