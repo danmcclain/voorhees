a5af68d7f15a6b90f50b64d2d17679871ffc37c5
Dan McClain <git@danmcclain.net>
2015-04-29 11:59:27 -0400

    Only normalizes expected keys passed to `matches_schema?`
    `Poison.decode!` always uses strings for keys, so it makes sense to only
    normalize the expected keys

--- lib/voorhees.ex
+++ lib/voorhees.ex
@@ -1,25 +1,24 @@
 defmodule Voorhees do
   require IEx
-  def matches_schema?(content, keys) do
+  def matches_schema?(content, expected_keys) do
     content_map =  Poison.decode!(content)
     content_keys = content_map
     |> Map.keys
     |> _extract_subkeys(content_map)
-    |> _normalize_keys
 
-    keys = _normalize_keys(keys)
+    expected_keys = _normalize_keys(expected_keys)
 
-    Enum.empty?(keys -- content_keys) and Enum.empty?(content_keys -- keys)
+    Enum.empty?(expected_keys -- content_keys) and Enum.empty?(content_keys -- expected_keys)
   end
 
   defp _normalize_keys([]), do: []
-  defp _normalize_keys([key|rest]) when is_atom(key), do: [key|_normalize_keys(rest)]
-  defp _normalize_keys([key|rest]) when is_binary(key), do: [String.to_atom(key)|_normalize_keys(rest)]
-  defp _normalize_keys([{key, subkeys}|rest]) when is_atom(key) and is_list(subkeys) do
+  defp _normalize_keys([key|rest]) when is_binary(key), do: [key|_normalize_keys(rest)]
+  defp _normalize_keys([key|rest]) when is_atom(key), do: [Atom.to_string(key)|_normalize_keys(rest)]
+  defp _normalize_keys([{key, subkeys}|rest]) when is_binary(key) and is_list(subkeys) do
     [{key, _normalize_keys(subkeys)}|_normalize_keys(rest)]
   end
-  defp _normalize_keys([{key, subkeys}|rest]) when is_binary(key) and is_list(subkeys) do
-    [{String.to_atom(key), _normalize_keys(subkeys)}|_normalize_keys(rest)]
+  defp _normalize_keys([{key, subkeys}|rest]) when is_atom(key) and is_list(subkeys) do
+    [{Atom.to_string(key), _normalize_keys(subkeys)}|_normalize_keys(rest)]
   end
 
   defp _extract_subkeys([], _map), do: []
