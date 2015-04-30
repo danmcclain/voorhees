6d3edaee0923d0aaa1204dc759eb44528ef54a71
Dan McClain <git@danmcclain.net>
2015-04-30 06:48:55 -0400

    Adds list checking for `matches_schema?`
     * Will check that lists of objects are validated against the expected keys
     * Will check for a list of scalar values when an empty list is in the
       expcted keys

--- lib/voorhees.ex
+++ lib/voorhees.ex
@@ -1,14 +1,27 @@
 defmodule Voorhees do
   require IEx
   def matches_schema?(content, expected_keys) do
-    content_map =  Poison.decode!(content)
-    content_keys = content_map
-    |> Map.keys
-    |> _extract_subkeys(content_map)
-
     expected_keys = _normalize_keys(expected_keys)
+    parsed_content =  Poison.decode!(content)
 
-    Enum.empty?(expected_keys -- content_keys) and Enum.empty?(content_keys -- expected_keys)
+    _matches_schema?(parsed_content, expected_keys)
+  end
+
+  defp _matches_schema?(content, expected_keys) when is_list(content) do
+    content
+    |> Enum.map(fn (element) -> _matches_schema?(element, expected_keys) end)
+    |> Enum.all?
+  end
+
+  defp _matches_schema?(content, expected_keys) when is_map(content) do
+    content_keys = content
+    |> Map.keys
+    |> _extract_subkeys(content)
+
+    extra_keys = content_keys -- expected_keys
+    missing_keys = expected_keys -- content_keys
+
+    Enum.empty?(extra_keys) and Enum.empty?(missing_keys)
   end
 
   defp _normalize_keys([]), do: []
@@ -25,6 +38,17 @@ defmodule Voorhees do
   defp _extract_subkeys([key|rest], map) do
     case map[key] do
       (value) when is_map(value) -> [{key, _extract_subkeys(Map.keys(value), value)}|_extract_subkeys(rest, map)]
+      (value) when is_list(value) ->
+        subkey_arrays = value
+        |> Enum.map(fn
+          (element) when is_map(element) -> _extract_subkeys(Map.keys(element), element)
+          (_) -> []
+        end)
+        |> Enum.uniq
+
+        if Enum.count(subkey_arrays) == 1, do: [subkey_arrays] = subkey_arrays
+
+        [{key, subkey_arrays}|_extract_subkeys(rest, map)]
       (_) -> [key|_extract_subkeys(rest, map)]
     end
   end
--- test/matches_schema_test.exs
+++ test/matches_schema_test.exs
@@ -7,6 +7,14 @@ defmodule Voorhees.Test.MatchesSchema do
     assert matches_schema?(content, [:a, "b"])
   end
 
+  test "empty lists in expected keys should be lists of scalars in content" do
+    content = Poison.encode! %{ a: 1, b: 2, c: [1, 2, 3, nil, "test"] }
+    assert matches_schema?(content, [:a, "b", c: []])
+
+    content = Poison.encode! %{ a: 1, b: 2, c: [1, %{ b: 1 }, 3] }
+    assert !matches_schema?(content, [:a, "b", c: []])
+  end
+
   test "returns false when a key is missing from the passed in value" do
     content = Poison.encode! %{ a: 1 }
     assert !matches_schema?(content, [:a, :b])
@@ -17,7 +25,7 @@ defmodule Voorhees.Test.MatchesSchema do
     assert !matches_schema?(content, [:a, :b])
   end
 
-  test "checks nested keys" do
+  test "checks nested object" do
     content = Poison.encode! %{ a: 1, b: 2, c: %{ d: 4 } }
     assert matches_schema?(content, [:a, :b, c: [:d]])
 
@@ -32,5 +40,27 @@ defmodule Voorhees.Test.MatchesSchema do
 
     content = Poison.encode! %{ a: 1, b: 2, c: %{ d: 4, e: %{ f: 7, g: 8 } } }
     assert !matches_schema?(content, [:a, :b, c: [:d, e: [:f]]])
+  end
+
+  test "checks lists" do
+    content = Poison.encode! [%{ a: 1, b: 2, c: %{ d: 4 } }]
+    assert matches_schema?(content, [:a, :b, c: [:d]])
+
+    content = Poison.encode! [%{ a: 1, b: 2, c: 3 }]
+    assert !matches_schema?(content, [:a, :b, c: [:d]])
+  end
+
+  test "check nested lists" do
+    content = Poison.encode! %{ a: 1, b: 2, c: [%{ d: 4 }, %{ d: 5 }] }
+    assert matches_schema?(content, [:a, :b, c: [:d]])
+
+    content = Poison.encode! [%{ a: 1, b: 2, c: [%{ d: 4 }] }]
+    assert matches_schema?(content, [:a, :b, c: [:d]])
+
+    content = Poison.encode! [%{ a: 1, b: 2, c: [%{ d: 4, e: 5 }] }]
+    assert !matches_schema?(content, [:a, :b, c: [:d]])
+
+    content = Poison.encode! [%{ a: 1, b: 2, c: [%{ d: 4 }, %{ d: 4, e: 5 }] }]
+    assert !matches_schema?(content, [:a, :b, c: [:d]])
   end
 end
