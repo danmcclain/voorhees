4bc80e9902ca045ed57a8c51ba673767e9f874fd
Brian Cardarella <bcardarella@gmail.com>
2015-08-31 10:07:21 -0400

    Adds support for schema contains assertions

--- lib/voorhees/json_api.ex
+++ lib/voorhees/json_api.ex
@@ -19,6 +19,33 @@ defmodule Voorhees.JSONApi do
     actual
   end
 
+  def assert_schema_contains(%{"data" => resource} = actual, expected) when is_map(resource) do
+    assert_schema_contains(put_in(actual["data"], [resource]), expected)
+  end
+  def assert_schema_contains(%{"data" => resources} = actual, expected) when is_list(resources) do
+    actual = Enum.map(resources ++ (actual["included"] || []), fn(resource) -> _stringify_keys(resource) end)
+    _assert_schema_contains(actual, _stringify_keys(expected))
+  end
+
+  defp _assert_schema_contains(actual, expected) do
+    actual_types = Enum.map(actual, fn(resource) -> resource["type"] end)
+    expected_types = Map.keys(expected)
+
+    assert length(expected_types -- actual_types) == 0,
+      "Expected types: #{Enum.join(expected_types, ", ")}\nGot: #{Enum.join(actual_types, ", ")}"
+
+    Enum.each expected_types, fn(expected_type) ->
+      expected_attributes = _stringify_items(expected[expected_type]["attributes"])
+      Enum.each(actual, fn(resource) ->
+        if resource["type"] == expected_type do
+          actual_attributes = Map.keys(resource["attributes"])
+          assert length(expected_attributes -- actual_attributes) == 0,
+            "Expected type: #{expected_type} to contain: #{Enum.join(expected_attributes, ", ")}\nGot: #{Enum.join(actual_attributes, ", ")}"
+        end
+      end)
+    end
+  end
+
   defp _assert_resource(resource, expected) do
     %{"type" => type, "attributes" => attributes} = resource
 
@@ -50,4 +77,21 @@ defmodule Voorhees.JSONApi do
 
     actual
   end
+
+  defp _stringify_items([]), do: []
+  defp _stringify_items([head|tail]) when is_atom(head) do
+    [Atom.to_string(head)|_stringify_items(tail)]
+  end
+  defp _stringify_items([head|tail]) when is_binary(head) do
+    [head|_stringify_items(tail)]
+  end
+
+  defp _stringify_keys(object) when is_map(object) do
+    Enum.into(object, %{}, &_stringify_key(&1))
+  end
+  defp _stringify_keys(object), do: object
+  defp _stringify_key({key, value}) when is_binary(key),
+    do: {key, _stringify_keys(value)}
+  defp _stringify_key({key, value}) when is_atom(key),
+    do: {Atom.to_string(key), _stringify_keys(value)}
 end
